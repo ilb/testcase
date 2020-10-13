a4a1065e43267d8bb4709124eca0b03e438525ed
safargalin <safargalin@bystrobank.ru>
2020-10-13 12:41:03 +0400

    fixed names

--- src/test/java/ru/ilb/testcase/AccrueTestCaseTest.java
+++ src/test/java/ru/ilb/testcase/AccrueTestCaseTest.java
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ru.ilb.debtaccounting.testcase;
+package ru.ilb.testcase;
 
-import ru.ilb.testcase.AccrueTestCase;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
--- src/test/java/ru/ilb/testcase/ValueConverterTest.java
+++ src/test/java/ru/ilb/testcase/ValueConverterTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ru.ilb.debtaccounting.testcase;
+package ru.ilb.testcase;
 
 import ru.ilb.testcase.ValueConverter;
 import org.junit.jupiter.api.Test;
